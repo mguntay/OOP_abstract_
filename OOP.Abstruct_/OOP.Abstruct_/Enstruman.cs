7de2f04fb6a19ae9b6217957d112e4a904b8efc0
mguntay <guntay11@gmail.com>
2019-12-28 12:44:36 +0300

    OOP.abstrtract

---
+++ OOP.Abstruct_/OOP.Abstruct_/Enstruman.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Abstruct_
+{
+    public abstract class Enstruman
+    {
+        public string Markasi { get; set; }
+        public string Aciklamasi { get; set; }
+        public abstract string Cal();
+
+    }
+    public class Bateri :Enstruman
+    {
+        public override string Cal()
+        {
+            return "Bateri Sesi";
+        }
+    }
+    public class Keman :Enstruman
+    {
+        public override string Cal()
+        {
+            return "Keman Sesi";
+        }
+    }
+
+}
